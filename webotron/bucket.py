3a82723ff8c1ba6c7f437b95b6d3a787750e2a6e
nawinkumar94 <nawinkumar94@gmail.com>
2020-08-31 23:08:16 +0530

    Hosting a website using s3 additionally configured route 53 and cloud front

---
+++ webotron/bucket.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+"""Class for s3 bucket."""
+from pathlib import Path
+from functools import reduce
+from botocore.exceptions import ClientError
+from boto3.s3.transfer import TransferConfig
+from hashlib import md5
+import mimetypes
+from webotron import util
+
+
+class BucketManager:
+    """Manage an S3 Bucket."""
+    CHUNK_SIZE = 8388608
+
+    def __init__(self, session):
+        """Create an BucketManager object."""
+        self.session = session
+        self.s3 = self.session.resource('s3')
+        self.transfer_config = TransferConfig(
+            multipart_chunksize=self.CHUNK_SIZE,
+            multipart_threshold=self.CHUNK_SIZE
+        )
+        self.manifest = {}
+
+    def all_buckets(self):
+        """Get an iterator all the buckets."""
+        return self.s3.buckets.all()
+
+    def all_objects(self, bucket_name):
+        """Get an iterator all the objects in bucket."""
+        return self.s3.Bucket(bucket_name).objects.all()
+
+    def init_bucket(self, bucket_name):
+        """Create a new bucket or return the name of the bucket if already exists."""
+        s3_bucket = None
+        try:
+            s3_bucket = self.s3.create_bucket(
+                    Bucket=bucket_name,
+                    CreateBucketConfiguration={
+                        'LocationConstraint': self.session.region_name})
+        except ClientError as error:
+            if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+                s3_bucket = self.s3.Bucket(bucket_name)
+            else:
+                raise error
+        return s3_bucket
+
+    def set_bucket_policy(self, bucket_name):
+        """Set bucket policy to the bucket."""
+        policy = """
+        {
+            "Id": "Policy1597808344458",
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "Stmt1597808341979",
+                    "Action": [
+                        "s3:GetObject"
+                    ],
+                    "Effect": "Allow",
+                    "Resource": "arn:aws:s3:::%s/*",
+                    "Principal": "*"
+                }
+            ]
+        }
+        """ % bucket_name
+        policy = policy.strip()
+        pol = self.s3.BucketPolicy(bucket_name)
+        pol.put(Policy=policy)
+
+    def bucket_website(self, bucket_name):
+        """Add the files in the bucket to host a website."""
+        bucket_website = self.s3.BucketWebsite(bucket_name)
+        bucket_website.put(WebsiteConfiguration={
+            'ErrorDocument': {
+                'Key': 'error.html'
+            },
+            'IndexDocument': {
+                'Suffix': 'index.html'
+            }
+        })
+
+    def get_region_name(self,bucket_name):
+        """Getting the location of the bucket."""
+        client = self.s3.meta.client
+        bucket_location=client.get_bucket_location(Bucket=bucket_name)
+        return bucket_location["LocationConstraint"] or 'us-east-1'
+
+    def get_bucket_url(self,bucket_name):
+        """Getting the URL of the bucket fro which the static website is hosted."""
+        if util.known_region(self.get_region_name(bucket_name)):
+            bucket_url="http://{}.{}".format(bucket_name,util.region_endpoint(self.get_region_name(bucket_name)).host)
+            return bucket_url
+        else:
+            return "invalid URL"
+
+    def load_manifest(self,bucket_name):
+        """Load the Etag for the files in the bucket to compare with Etag of uploading file."""
+        paginator=self.s3.meta.client.get_paginator('list_objects_v2')
+        for page in paginator.paginate(Bucket= bucket_name):
+            for obj in page.get('Contents',[]):
+                self.manifest[obj['Key']]=obj['ETag']
+
+
+    @staticmethod
+    def hash_data(data):
+        """Generate hash in md5 format for the data."""
+        hash = md5()
+        hash.update(data)
+        return hash;
+
+    def generate_etag(self, path):
+        """Generate Etag for the uploading files based on the CHUNK size
+            and compare with of files in bucket."""
+        hashes = []
+        with open(path, 'rb') as file:
+            while True:
+                #Read the data upto the chunk size.
+                data =  file.read(self.CHUNK_SIZE)
+                # Loop becomes fales if teh data size is more than chunck size.
+                if not data:
+                    break
+
+                hashes.append(self.hash_data(data))
+
+        if not hashes:
+            return
+        elif len(hashes) == 1:
+            return '"{}"'.format(hashes[0].hexdigest())
+        else:
+            # If the file size is greater than chunk size AWS uses multipart upload api,
+            # append all the tag with size of the file.
+            hash = self.hash_data(reduce(lambda x, y: x + y, (h.digest() for h in hashes)))
+            return '"{}-{}"'.format(hash.hexdigest(), len(hashes))
+
+
+    def upload_file(self, bucket, path, key):
+        """Upload the file in to s3 bucket with path and key."""
+        content_type = mimetypes.guess_type(key)[0] or 'text/html'
+        etag_upload_file = self.generate_etag(path)
+        print('etag_upload_file  {} '.format(etag_upload_file))
+        print('self.manifest.get(key)  {} '.format(self.manifest.get(key)))
+        if self.manifest.get(key, '') == etag_upload_file:
+            print("Skipping already uploaded key {} :".format(etag_upload_file))
+            return
+
+        return bucket.upload_file(
+            path,
+            key,
+            ExtraArgs={
+                'ContentType': content_type
+            },
+            Config=self.transfer_config
+            )
+
+
+    def sync_bucket(self, pathname, bucket_name):
+        """Sync the contents of the path name to bucket."""
+        root = Path(pathname).expanduser().resolve()
+        bucket = self.s3.Bucket(bucket_name)
+        self.load_manifest(bucket_name)
+        def handle_directory(target):
+            for path_value in target.iterdir():
+                if path_value.is_dir():
+                    handle_directory(path_value)
+                if path_value.is_file():
+                    self.upload_file(bucket,
+                                str(path_value.as_posix()),
+                                str(path_value.relative_to(root).as_posix()))
+
+        handle_directory(root)
---
+++ webotron/webotron.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Webotron Deploy the websites with AWS.
+
+Webotron automates the process of deploying the static website hosting
+- Configure AWS S3 buckets
+    -Create them
+    -Set them for static website hosting
+    -Deploy the local files in to them
+- Configure DNS with AWS Route53
+- Configure the Content Delivery Network and SSL with AWS.
+"""
+
+import boto3
+import click
+from webotron import util
+from webotron.bucket import BucketManager
+from webotron.domain import DomainManager
+from webotron.certificate import CertificateManager
+from webotron.cdn import DistributionManager
+
+
+session = None
+bucket_manager = None
+domain_manager = None
+cert_manager = None
+dist_manager = None
+
+
+@click.group()
+@click.option('--profile', default=None,
+                help= "Add profile for the credentials")
+def cli(profile):
+    """Grop all the click commands."""
+    global session, bucket_manager, domain_manager, cert_manager, dist_manager
+    session_data = {}
+    if profile:
+        session_data['profile_name'] = profile
+    session = boto3.Session(**session_data)
+    bucket_manager = BucketManager(session)
+    domain_manager = DomainManager(session)
+    cert_manager = CertificateManager(session)
+    dist_manager = DistributionManager(session)
+
+
+@cli.command('list-buckets')
+def list_buckets():
+    """List all the buckets in S3."""
+    for bucket in bucket_manager.all_buckets():
+        print(bucket)
+
+
+@cli.command('list-buckets-objects')
+@click.argument('bucket_name')
+def list_buckets_objects(bucket_name):
+    """List all the objects in S3 buckets."""
+    for object in bucket_manager.all_objects(bucket_name):
+        print(object)
+
+
+@cli.command('setup-bucket')
+@click.argument('bucket_name')
+def setup_bucket(bucket_name):
+    """Set up bucket for hosting a website also added the bucket policy."""
+    s3_bucket = bucket_manager.init_bucket(bucket_name)
+    bucket_manager.set_bucket_policy(bucket_name)
+    bucket_manager.bucket_website(bucket_name)
+
+
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket_name')
+def sync(pathname, bucket_name):
+    """Sync the contents of the path name to bucket."""
+    bucket_manager.sync_bucket(pathname, bucket_name)
+    print("The URL of hosted bucket is : "
+                    + bucket_manager.get_bucket_url(bucket_name))
+
+
+@cli.command('setup-domain')
+@click.argument('domain_name')
+def setup_domain(domain_name):
+    """Set the domain name for the bucket."""
+    # since both bucket and domain should be a same,setting bucket name to domain_name
+    bucket_name = domain_name
+    zone = domain_manager.find_hosted_zone(domain_name) \
+            or domain_manager.create_hosted_zone(domain_name)
+    endpoint = util.region_endpoint(bucket_manager.get_region_name(bucket_name))
+    domain_manager.create_s3_domain_record(zone, domain_name, endpoint)
+
+
+@cli.command('find-cert')
+@click.argument('domain_name')
+def find_cert(domain_name):
+    """Find a certificate for <DOMAIN>."""
+    print(cert_manager.find_matching_cert(domain_name))
+
+
+@cli.command('setup-cdn')
+@click.argument('domain')
+@click.argument('bucket')
+def setup_cdn(domain, bucket):
+    """Set up CloudFront CDN for DOMAIN pointing to BUCKET."""
+    dist = dist_manager.find_matching_dist(domain)
+
+    if not dist:
+        cert = cert_manager.find_matching_cert(domain)
+        if not cert:  # SSL is not optional at this time
+            print("Error: No matching cert found.")
+            return
+
+        dist = dist_manager.create_dist(domain, cert)
+        print("Waiting for distribution deployment...")
+        dist_manager.await_deploy(dist)
+
+    zone = domain_manager.find_hosted_zone(domain) \
+        or domain_manager.create_hosted_zone(domain)
+
+    domain_manager.create_cf_domain_record(zone, domain, dist['DomainName'])
+    print("Domain configured: https://{}".format(domain))
+
+    return
+
+
+if __name__ == '__main__':
+    cli()
